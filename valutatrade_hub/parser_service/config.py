adcb13391b7b4a9419501e7098abbf84b70532de
Elena Nosulchak <133764080+NosulchakE@users.noreply.github.com>
2025-11-11 20:57:12 +0300

    Update config.py

--- valutatrade_hub/parser_service/config.py
+++ valutatrade_hub/parser_service/config.py
@@ -1,24 +1,25 @@
 import os
 from dataclasses import dataclass
+from dotenv import load_dotenv
+
+# Загружаем .env
+load_dotenv()
 
 @dataclass
 class ParserConfig:
-    # Ключ ExchangeRate-API
-    EXCHANGERATE_API_KEY: str = os.getenv("EXCHANGERATE_API_KEY", "1a4a95b327278c18d57643bb")
-
-    # URL
-    EXCHANGERATE_API_URL: str = "https://v6.exchangerate-api.com/v6"
-
-    # Списки валют
+    EXCHANGERATE_API_KEY: str = os.getenv("EXCHANGERATE_API_KEY")
     BASE_CURRENCY: str = "USD"
     FIAT_CURRENCIES: tuple = ("EUR", "GBP", "RUB")
     CRYPTO_CURRENCIES: tuple = ("BTC", "ETH", "SOL")
-
-    # Пути к JSON
+    CRYPTO_ID_MAP: dict = {
+        "BTC": "bitcoin",
+        "ETH": "ethereum",
+        "SOL": "solana",
+    }
     RATES_FILE_PATH: str = "data/rates.json"
     HISTORY_FILE_PATH: str = "data/exchange_rates.json"
-
-    # Сетевые параметры
     REQUEST_TIMEOUT: int = 10
+
+
 
 
