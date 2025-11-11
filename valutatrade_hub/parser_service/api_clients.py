3e6921add0e78bf14732be20d42933e73a2178e2
Elena Nosulchak <133764080+NosulchakE@users.noreply.github.com>
2025-11-11 17:13:41 +0300

    Update api_clients.py

--- valutatrade_hub/parser_service/api_clients.py
+++ valutatrade_hub/parser_service/api_clients.py
@@ -1,33 +1,33 @@
-# valutatrade_hub/parser_service/api_clients.py
 import requests
-from datetime import datetime
-from valutatrade_hub.core.exceptions import ApiRequestError
 from .config import ParserConfig
+from valutatrade_hub.core.exceptions import ApiRequestError
 
 class ExchangeRateApiClient:
-    """Клиент для ExchangeRate-API"""
     def __init__(self, config: ParserConfig):
         self.config = config
 
     def fetch_rates(self):
         url = f"{self.config.EXCHANGERATE_API_URL}/{self.config.EXCHANGERATE_API_KEY}/latest/{self.config.BASE_CURRENCY}"
         try:
-            response = requests.get(url, timeout=self.config.REQUEST_TIMEOUT)
-            response.raise_for_status()
-            data = response.json()
+            resp = requests.get(url, timeout=self.config.REQUEST_TIMEOUT)
+            resp.raise_for_status()
+            data = resp.json()
             if data.get("result") != "success":
-                raise ApiRequestError(f"ExchangeRate-API returned failure: {data}")
+                raise ApiRequestError(f"ExchangeRate-API error: {data}")
             
             rates = {}
-            updated_at = datetime.strptime(data["time_last_update_utc"], "%a, %d %b %Y %H:%M:%S +0000").isoformat() + "Z"
-            for code, rate in data["rates"].items():
-                if code.upper() in self.config.FIAT_CURRENCIES + self.config.CRYPTO_CURRENCIES:
-                    key = f"{code.upper()}_{self.config.BASE_CURRENCY}"
-                    rates[key] = {
-                        "rate": rate,
-                        "updated_at": updated_at,
-                        "source": "ExchangeRate-API"
-                    }
+            updated_at = data.get("time_last_update_utc")
+            for code in self.config.FIAT_CURRENCIES + self.config.CRYPTO_CURRENCIES:
+                if code not in data["rates"]:
+                    continue
+                rates[f"{code}_USD"] = {
+                    "rate": data["rates"][code],
+                    "updated_at": updated_at,
+                    "source": "ExchangeRate-API"
+                }
             return rates
-        except requests.exceptions.RequestException as e:
-            raise ApiRequestError(f"Ошибка при обращении к ExchangeRate-API: {e}") from e
+
+        except requests.RequestException as e:
+            raise ApiRequestError(f"Network error: {e}")
+
+
