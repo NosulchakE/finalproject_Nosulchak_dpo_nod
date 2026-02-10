1fadc8ce0d23fbe5c225853433615612f6c2331d
Elena Nosulchak <133764080+NosulchakE@users.noreply.github.com>
2026-02-10 15:57:11 +0300

    приводим в соответствие по логам

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 # main.py
+import logging
 from valutatrade_hub.cli.interface import run_interactive_cli
 
 
-def print_banner():
-    print("CLI для управления валютным портфелем.\n")
+def setup_logging():
+    """
+    настройка логирования для всего приложения.
+    """
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s [%(levelname)s] %(name)s: %(message)s"
+    )
+
 
 def main():
-    print_banner()
+    setup_logging()
+    logger = logging.getLogger("Main")
+
+    logger.info("Запуск CLI для управления валютным портфелем")
     run_interactive_cli()
+
 
 if __name__ == "__main__":
     main()
+
 
 
 
