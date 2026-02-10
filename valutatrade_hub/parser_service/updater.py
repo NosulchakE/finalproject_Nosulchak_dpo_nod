45190aac84878417fb958f49383ced45ece9b836
Elena Nosulchak <133764080+NosulchakE@users.noreply.github.com>
2026-02-10 12:28:48 +0300

    исправление ошибки, добавила работу через заглушку

--- valutatrade_hub/parser_service/updater.py
+++ valutatrade_hub/parser_service/updater.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from datetime import datetime, timezone
 from typing import Dict, Any
 
-from .api_clients import ExchangeRateAPI
+from .api_clients import BaseApiClient, ExchangeRateApiClient
 from .storage import RatesStorage
 from .config import ParserConfig
 from valutatrade_hub.core.exceptions import ApiRequestError
@@ -16,89 +16,61 @@ class RatesUpdater:
     def __init__(self, source: str = None):
         self.config = ParserConfig()
         self.source = source or "exchangerate-api"
-        self.api_client = ExchangeRateAPI()
+        self.api_client: BaseApiClient = ExchangeRateApiClient()
         self.storage = RatesStorage()
         
     def run_update(self) -> int:
-        """
-        Основной метод обновления курсов
-        """
+        """Основной метод обновления курсов"""
+        print(f"обновление курсов валют из {self.source}...")
         try:
-            print(f"🔄 Обновление курсов валют из {self.source}...")
-            
-            # Получаем свежие курсы от API
-            fresh_rates = self.api_client.get_rates()
-            
-            if not fresh_rates:
-                print(" Не получены данные от API")
-                return 0
-            
-            # Обновляем локальный кэш (rates.json)
-            updated_count = self._update_rates_cache(fresh_rates)
-            
-            # Сохраняем исторические данные (exchange_rates.json)
-            self._save_historical_data(fresh_rates)
-            
-            print(f" Обновлено {updated_count} курсов валют")
-            return updated_count
-            
-        except Exception as e:
-            print(f" Ошибка при обновлении курсов: {e}")
-            raise ApiRequestError(f"Ошибка API: {e}")
+            fresh_rates = self.api_client.fetch_rates()
+        except ApiRequestError as e:
+            print(f" Ошибка API ({self.source}): {e}. Используем тестовые данные")
+            fresh_rates = self.api_client.fetch_rates()  # заглушка внутри клиента в случае некорректной работы  API
+
+        if not fresh_rates:
+            print(" Не получены данные от API и заглушка пустая")
+            return 0
+        
+        updated_count = self._update_rates_cache(fresh_rates)
+        self._save_historical_data(fresh_rates)
+        
+        print(f" обновлено {updated_count} курсов валют")
+        return updated_count
     
     def _update_rates_cache(self, fresh_rates: Dict[str, Any]) -> int:
-        """
-        Обновляет файл rates.json (локальный кэш для Core Service)
-        """
+        """Обновляет файл rates.json (локальный кэш для Core Service)"""
         rates_file = Path(self.config.RATES_FILE_PATH)
-        rates_file.parent.mkdir(exist_ok=True)
+        rates_file.parent.mkdir(parents=True, exist_ok=True)
         
-        rates_data = {
-            "pairs": {},
-            "last_refresh": datetime.now(timezone.utc).isoformat()
-        }
-        
-        updated_count = 0
         base_currency = self.config.BASE_CURRENCY
-        
-        # Фильтруем только нужные нам валюты
         target_currencies = set(self.config.FIAT_CURRENCIES + self.config.CRYPTO_CURRENCIES)
+        last_refresh = datetime.now(timezone.utc).isoformat()
         
+        pairs = {}
+        updated_count = 0
+
         for currency, rate in fresh_rates.items():
             if currency in target_currencies and currency != base_currency:
-                # Прямая пара: BASE -> Currency
-                pair_key = f"{base_currency}_{currency}"
-                rates_data["pairs"][pair_key] = {
-                    "rate": rate,
-                    "updated_at": rates_data["last_refresh"],
-                    "source": "ExchangeRate-API"
-                }
+                pairs[f"{base_currency}_{currency}"] = {"rate": rate, "updated_at": last_refresh, "source": self.source}
                 updated_count += 1
-                
-                # Обратная пара: Currency -> BASE
                 if rate != 0:
-                    pair_key = f"{currency}_{base_currency}"
-                    rates_data["pairs"][pair_key] = {
-                        "rate": 1 / rate,
-                        "updated_at": rates_data["last_refresh"],
-                        "source": "ExchangeRate-API"
-                    }
+                    pairs[f"{currency}_{base_currency}"] = {"rate": 1 / rate, "updated_at": last_refresh, "source": self.source}
                     updated_count += 1
         
-        # Сохраняем в файл
+        rates_data = {"pairs": pairs, "last_refresh": last_refresh}
         with open(rates_file, 'w', encoding='utf-8') as f:
-            json.dump(rates_data, f, indent=2, ensure_ascii=False)
+            json.dump(rates_data, f, ensure_ascii=False, indent=2)
         
         return updated_count
     
     def _save_historical_data(self, fresh_rates: Dict[str, Any]):
-        """
-        Сохраняет исторические данные в exchange_rates.json
-        """
+        """Сохраняет исторические данные в exchange_rates.json"""
         try:
             self.storage.save_rates(fresh_rates)
         except Exception as e:
             print(f" Не удалось сохранить исторические данные: {e}")
+
 
 
 
