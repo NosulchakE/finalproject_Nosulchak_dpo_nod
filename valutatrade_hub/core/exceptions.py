d951b5912e59652ccd5383ee052f500e4b807033
Elena Nosulchak <133764080+NosulchakE@users.noreply.github.com>
2026-02-10 21:30:18 +0300

    исправление ошибки по данным ТЗ

--- valutatrade_hub/core/exceptions.py
+++ valutatrade_hub/core/exceptions.py
@@ -1,10 +1,35 @@
 class InsufficientFundsError(Exception):
-    pass
+    """Недостаточно средств в кошельке"""
+
+    def __init__(self, available: float, required: float, code: str):
+        self.available = available
+        self.required = required
+        self.code = code
+
+        message = (
+            f"Недостаточно средств: "
+            f"доступно {available:.4f} {code}, "
+            f"требуется {required:.4f} {code}"
+        )
+        super().__init__(message)
+
 
 class CurrencyNotFoundError(Exception):
-    pass
+    """Неизвестная валюта"""
+
+    def __init__(self, code: str):
+        self.code = code
+        message = f"Неизвестная валюта '{code}'"
+        super().__init__(message)
+
 
 class ApiRequestError(Exception):
-    pass
+    """Ошибка при обращении к внешнему API"""
+
+    def __init__(self, reason: str):
+        self.reason = reason
+        message = f"Ошибка при обращении к внешнему API: {reason}"
+        super().__init__(message)
+
 
 
