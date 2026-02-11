e567fc4cf9d6590a4fb2ac2f2aa975f1a9da1704
Elena Nosulchak <133764080+NosulchakE@users.noreply.github.com>
2026-02-11 11:49:19 +0300

    обновили по изменениям

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
 # main.py
-import logging
+from valutatrade_hub.logging_config import setup_logging
 from valutatrade_hub.cli.interface import run_interactive_cli
-from valutatrade_hub.logging_config import setup_logging  # ИСПОЛЬЗУЕМ ОБЩУЮ НАСТРОЙКУ
-
 
 def main():
-    # Настройка логирования для всего приложения
-    setup_logging()
-    
-    #  логгер после настройки
-    logger = logging.getLogger("Main")
-    logger.info("Запуск CLI для управления валютным портфелем")
-    
+    setup_logging() 
     run_interactive_cli()
-
 
 if __name__ == "__main__":
     main()
