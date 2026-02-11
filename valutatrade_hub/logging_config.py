4a4a5e483f40eef341b54bdc0e4d5b34d3a77c60
Elena Nosulchak <133764080+NosulchakE@users.noreply.github.com>
2026-02-11 10:57:17 +0300

    добавляем консольный обработчик,

--- valutatrade_hub/logging_config.py
+++ valutatrade_hub/logging_config.py
@@ -1,19 +1,38 @@
+# valutatrade_hub/logging_config.py
 import logging
 from logging.handlers import RotatingFileHandler
 from pathlib import Path
 
-LOG_FILE = Path("logs/actions.log")
-LOG_FILE.parent.mkdir(exist_ok=True, parents=True)  # Создать папку logs, если нет
+def setup_logging():
+    """логированиу для всего приложения"""
+    LOG_FILE = Path("logs/actions.log")
+    LOG_FILE.parent.mkdir(exist_ok=True, parents=True)
+    
+    # Настройка корневого логгера (а не только "valutatrade")
+    root_logger = logging.getLogger()
+    root_logger.setLevel(logging.INFO)
+    
+    # Форматтер 
+    formatter = logging.Formatter(
+        "%(asctime)s %(levelname)s %(message)s", 
+        datefmt="%Y-%m-%dT%H:%M:%S"
+    )
+    
+    # Файловый обработчик 
+    file_handler = RotatingFileHandler(
+        LOG_FILE, maxBytes=1_000_000, backupCount=5, encoding="utf-8"
+    )
+    file_handler.setFormatter(formatter)
+    file_handler.setLevel(logging.INFO)
+    
+    #  консольный обработчик
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    console_handler.setLevel(logging.INFO)
+    
+    # Убираем старые обработчики и добавляем новые
+    root_logger.handlers.clear()
+    root_logger.addHandler(file_handler)
+    root_logger.addHandler(console_handler)
 
-# Настройка логгера
-logger = logging.getLogger("valutatrade")
-logger.setLevel(logging.INFO)
 
-formatter = logging.Formatter(
-    "%(asctime)s %(levelname)s %(message)s", datefmt="%Y-%m-%dT%H:%M:%S"
-)
-
-# Ротация: максимум 5 файлов по 1MB каждый
-handler = RotatingFileHandler(LOG_FILE, maxBytes=1_000_000, backupCount=5, encoding="utf-8")
-handler.setFormatter(formatter)
-logger.addHandler(handler)
