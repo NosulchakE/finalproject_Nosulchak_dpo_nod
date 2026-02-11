3459062618ed0ee7017842389b92ce4e85d636ab
Elena Nosulchak <133764080+NosulchakE@users.noreply.github.com>
2026-02-11 11:12:26 +0300

    исправление ошибки

--- valutatrade_hub/parser_service/config.py
+++ valutatrade_hub/parser_service/config.py
@@ -7,23 +7,23 @@ from typing import Dict, Tuple
 class ParserConfig:
     # Ключ загружается из переменной окружения
     EXCHANGERATE_API_KEY: str = field(default_factory=lambda: os.getenv("EXCHANGERATE_API_KEY", ""))
-
+    
     # Эндпоинт ExchangeRate-API
     EXCHANGERATE_API_URL: str = "https://v6.exchangerate-api.com/v6"
-
+    
     # Списки валют
     BASE_CURRENCY: str = "USD"
     FIAT_CURRENCIES: Tuple[str, ...] = ("EUR", "GBP", "RUB")
     CRYPTO_CURRENCIES: Tuple[str, ...] = ("BTC", "ETH", "SOL")
-
-    # Пути
-    RATES_FILE_PATH: str = "data/rates.json"
-    HISTORY_FILE_PATH: str = "data/exchange_rates.json"
-
+    
+    # Пути  оставляем относительные пути)
+    RATES_FILE_PATH: str = field(default_factory=lambda: "data/rates.json")
+    HISTORY_FILE_PATH: str = field(default_factory=lambda: "data/exchange_rates.json")
+    
     # Сетевые параметры
     REQUEST_TIMEOUT: int = 10
-
-    # Для возможной кастомизации (например, сопоставления тикеров, если понадобится)
+    
+    # Для возможной кастомизации
     CUSTOM_CRYPTO_MAP: Dict[str, str] = field(default_factory=dict)
 
 
