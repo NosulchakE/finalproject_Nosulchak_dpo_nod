86da2b0518bcecd59bcd75d8259ab6667fe3fac8
Elena Nosulchak <133764080+NosulchakE@users.noreply.github.com>
2026-02-11 10:51:34 +0300

    исправление логгирования

--- main.py
+++ main.py
@@ -1,23 +1,17 @@
 # main.py
 import logging
 from valutatrade_hub.cli.interface import run_interactive_cli
-
-
-def setup_logging():
-    """
-    настройка логирования для всего приложения.
-    """
-    logging.basicConfig(
-        level=logging.INFO,
-        format="%(asctime)s [%(levelname)s] %(name)s: %(message)s"
-    )
+from valutatrade_hub.logging_config import setup_logging  # ИСПОЛЬЗУЕМ ОБЩУЮ НАСТРОЙКУ
 
 
 def main():
+    # Настройка логирования для всего приложения
     setup_logging()
+    
+    #  логгер после настройки
     logger = logging.getLogger("Main")
-
     logger.info("Запуск CLI для управления валютным портфелем")
+    
     run_interactive_cli()
 
 
